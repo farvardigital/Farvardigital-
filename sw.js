918d40c293105c38e08da772f332fc14c0751da3
farvardigital <farvardigital@gmail.com>
2026-03-11 13:30:38 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,31 @@
+const CACHE_NAME = 'vof-armor-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/armor.html',
+  '/manifest.json'
+];
+
+// ഇൻസ്റ്റാൾ ചെയ്യുമ്പോൾ ഫയലുകൾ സേവ് ചെയ്യാൻ
 self.addEventListener('install', (e) => {
-  console.log('Vof Armor Installed');
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+  console.log('Vof Armor Service Worker Installed');
 });
+
+// ആപ്പ് സജീവമായി നിലനിർത്താൻ
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
+
+// ബാക്ക്ഗ്രൗണ്ടിൽ സിങ്ക് ചെയ്യാൻ (ഇത് ആപ്പ് ഓഫ് ആവാതിരിക്കാൻ സഹായിക്കും)
+self.addEventListener('periodicsync', (event) => {
+  console.log('Keeping Armor Active in Background');
 });
